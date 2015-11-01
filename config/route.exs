f99175ec8768b971d88c016eb8493579a9f4f847
Mickael Remond <mremond@process-one.net>
2015-11-01 16:03:44 +0100

    A single event should be able to match multiple streams

--- config/route.exs
+++ config/route.exs
@@ -10,18 +10,22 @@ defmodule HelloFast.Router do
   #end
 
   # previous code should generate:
-
+  
   # TODO: Check if pipeline length is < 0 and do not generate stream
   def streams do
     stream1 = {:localhost, [Stream.rate(5), Stream.stdout]}
     [stream1]
   end
+
+  def stream(event) do
+    streams |>
+      Enum.each( fn({name, _pipeline}) -> do_stream(name, event) end)
+  end
   
-  def stream(event = %Event{host: "localhost"}) do
-    name = :localhost
+  def do_stream(name = :localhost, event = %Event{host: "localhost"}) do
     send name, event
   end
   # Catch all case: We have no stream matching that event
-  def stream(_), do: :do_nothing
+  def do_stream(_, _), do: :do_nothing
 
 end
