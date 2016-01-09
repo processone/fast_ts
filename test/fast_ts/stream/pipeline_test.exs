6cf01fe65451194e4a48ab9e61aa8f97263c4253
Mickael Remond <mremond@process-one.net>
2016-01-09 12:51:00 +0100

    Initial tests

--- test/fast_ts/stream/pipeline_test.exs
+++ test/fast_ts/stream/pipeline_test.exs
@@ -2,12 +2,23 @@ defmodule FastTS.Stream.PipelineTest do
   use ExUnit.Case
 
   test "Ignore empty pipeline" do
-    assert FastTS.Stream.Pipeline.start_link(:test, []) == :ignore
+    assert FastTS.Stream.Pipeline.start_link(:empty, []) == :ignore
   end
-  
-#  test "Create pipeline one step" do
-#    create_fun = fn()
-#    {:ok, Pid} = FastTS.Stream.Pipeline.start_link(:test, [])
-#  end
+
+  test "Create a one step pipeline" do
+    start_fun = fn(ets_table, next_pid) ->
+      send test_pid, {:from, :started, self}
+      fn(event) -> event end
+    end
+    
+    {:ok, pid} = FastTS.Stream.Pipeline.start_link(:pipe1, [start_fun])
+    
+    receive do
+      {:from, :started, step1_pid} ->
+        assert Process.alive?(step1_pid)
+    after 5000 ->
+        assert false, "pipeline process not properly started"
+    end
+  end
 
 end
